75979500eff5a0462db06497fd1d181ce96f2c49
Fabian27 <fabianpf27@gmail.com>
2023-04-11 11:34:01 -0500

    chore: initial server configuration

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('hello!');
+import express from 'express';
+import { config as dotEnvConfig } from 'dotenv';
+dotEnvConfig();
+
+import { config } from './config';
+
+function boostrap() {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use('/api/health', (_, res) => {
+    res.send('checked');
+  });
+
+  const { port } = config.server;
+
+  app.listen(port, () => {
+    console.log(`Starting application on ${port}`);
+  });
+}
+
+boostrap();
