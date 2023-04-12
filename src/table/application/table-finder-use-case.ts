65c43e50cab5016918a2a464a8ccd3963e8dc519
Fabian27 <fabianpf27@gmail.com>
2023-04-12 10:17:37 -0500

    refactor: delete console log

--- src/table/application/table-finder-use-case.ts
+++ src/table/application/table-finder-use-case.ts
@@ -5,7 +5,6 @@ export class TableFinder {
   constructor(private readonly _tableRepository: TableRepository) {}
 
   async run(location?: number, capacity?: number): Promise<Table[]> {
-    console.log('app' + location, capacity);
     const tablas = await this._tableRepository.tableFinder(location, capacity);
     return tablas;
   }
