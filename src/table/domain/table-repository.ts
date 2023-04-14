fec78ba746f2f835f47578f5febac65c280d7df2
williamFabian <fabiandel541@gmail.com>
2023-04-14 14:55:33 -0500

    build: findById added to interface

--- src/table/domain/table-repository.ts
+++ src/table/domain/table-repository.ts
@@ -3,4 +3,5 @@ import { Table } from './table';
 export interface TableRepository {
   save(table: Table): Promise<Table>;
   tableFinder(location?: number, capacity?: number): Promise<Table[]>;
+  findById(id: string | number): Promise<Table | null>;
 }
