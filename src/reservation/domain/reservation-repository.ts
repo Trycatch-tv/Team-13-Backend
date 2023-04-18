a4a82fc6c6f90da11877728fa925897352955d5f
williamFabian <fabiandel541@gmail.com>
2023-04-18 10:33:53 -0500

    Deleted wrong commit

--- src/reservation/domain/reservation-repository.ts
+++ src/reservation/domain/reservation-repository.ts
@@ -3,8 +3,6 @@ import { Reservation, ReservationDto } from './reservation';
 
 export interface ReservationRepository {
 
-  reservationFinder(client?: string, number_people?:number, table?: Table): Promise<Reservation[]>;
-  
   findReservationById(
     idReservation: string | number
   ): Promise<Reservation | null>;
