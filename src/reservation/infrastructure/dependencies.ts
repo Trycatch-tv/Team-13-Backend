a21176681b00a6bb38e9e1a690705d8bc0389b7e
williamFabian <fabiandel541@gmail.com>
2023-04-17 23:11:57 -0500

    refactor: dependencie added for ReservationUpdate

--- src/reservation/infrastructure/dependencies.ts
+++ src/reservation/infrastructure/dependencies.ts
@@ -2,9 +2,15 @@ import { InMemoryReservationRepository } from './database/in-memory-reservation-
 
 import { ReservationFindById } from '../aplication/reservation-find-Id-use-case';
 import { ReservationFindByIdGetController } from './http/controller/reservation-find-id-get-controller';
+import { ReservationUpdate } from '../aplication/reservation-update-use-case';
+import { ReservationUpdateGetController } from './http/controller/reservation-update-get-controller';
 
 const dbReservationRepository = new InMemoryReservationRepository();
 
 const reservationFindById = new ReservationFindById(dbReservationRepository);
 export const reservationFindByIdController =
   new ReservationFindByIdGetController(reservationFindById);
+
+const reservationUpdated = new ReservationUpdate(dbReservationRepository);
+export const reservationUpdateGetController =
+  new ReservationUpdateGetController(reservationUpdated);
