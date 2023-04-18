69efdb3e76b038794c4284477e57ce272adc6742
williamFabian <fabiandel541@gmail.com>
2023-04-18 09:54:37 -0500

    Api reservationFinder (uncompleted)

--- src/reservation/domain/reservation-repository.ts
+++ src/reservation/domain/reservation-repository.ts
@@ -1,6 +1,10 @@
+import { Table } from '../../table/domain/table';
 import { Reservation, ReservationDto } from './reservation';
 
 export interface ReservationRepository {
+
+  reservationFinder(client?: string, number_people?:number, table?: Table): Promise<Reservation[]>;
+
   findReservationById(
     idReservation: string | number
   ): Promise<Reservation | null>;
---
+++ src/reservation/infrastructure/http/controller/reservation-finder-get-controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import status from 'http-status';
+
+import { ReservationFinder } from '../../../aplication/reservation-finder-use-case';
+import { ApiResponse } from '../../../../shared/api-response';
+
+export class ReservationFinderGetController{
+    constructor( private readonly _reservationFinder: ReservationFinder){}
+
+    async run() // Continuar código. 
+}
