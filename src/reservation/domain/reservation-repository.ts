b75533022e60e75a8d50421889b279eb97111840
williamFabian <fabiandel541@gmail.com>
2023-04-17 23:11:21 -0500

    refactor: updateReservation added to repository

--- src/reservation/domain/reservation-repository.ts
+++ src/reservation/domain/reservation-repository.ts
@@ -4,4 +4,9 @@ export interface ReservationRepository {
   findReservationById(
     idReservation: string | number
   ): Promise<Reservation | null>;
+
+  updateReservation(
+    updateReservationDto: ReservationDto,
+    id: string | number
+  ): Promise<Reservation>;
 }
