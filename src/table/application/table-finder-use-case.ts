f773d7d248118c4c41c63e9913ff8c6e6bf21620
Fabian27 <fabianpf27@gmail.com>
2023-04-28 12:12:34 -0500

    feat: add table number filter

--- src/table/application/table-finder-use-case.ts
+++ src/table/application/table-finder-use-case.ts
@@ -4,8 +4,16 @@ import { TableRepository } from '../domain/table-repository';
 export class TableFinder {
   constructor(private readonly _tableRepository: TableRepository) {}
 
-  async run(location?: number, capacity?: number): Promise<Table[]> {
-    const tablas = await this._tableRepository.tableFinder(location, capacity);
+  async run(
+    location?: number,
+    capacity?: number,
+    number?: number
+  ): Promise<Table[]> {
+    const tablas = await this._tableRepository.tableFinder(
+      location,
+      capacity,
+      number
+    );
     return tablas;
   }
 }
